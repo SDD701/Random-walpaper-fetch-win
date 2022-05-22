aa22c83af48966de0cc2ebd7c431dfeea7310819
SDD701 <19keriksov@protonmail.com>
2022-05-22 22:22:08 +0700

    draft

--- Parser.py
+++ Parser.py
@@ -1,6 +1,27 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://wall.alphacoders.com/random.php"
 page = requests.get(URL)
 
-print(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
+
+pictureLines = soup.find_all("div", class_="boxgrid")
+
+line = str(pictureLines[0])
+
+wallpaperId = line.split("=")
+wallpaperId = wallpaperId[3].split('"')
+wallpaperId = wallpaperId[0]
+
+print(wallpaperId)
+
+URL = "https://wall.alphacoders.com/wallpaper.php?i=" + wallpaperId + "&w=1920&h=1080&type=stretch"
+imageLink2 = "https://wall.alphacoders.com/big.php?i=" + wallpaperId
+print(URL)
+
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+print(soup)
