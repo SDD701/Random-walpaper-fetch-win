8fea21197615cb5379267def772d7c972119112c
SDD701 <19keriksov@protonmail.com>
2022-05-22 17:32:55 +0700

    Forgot the plan

---
+++ SetWallpaper.bat
@@ -0,0 +1,13 @@
+:: 1. Set wallpaper path
+Set WallpaperPath = %1
+:: 2. Go to https://wall.alphacoders.com/random.php
+:: 3. Parce the first link from it
+:: 4. Edit link:
+:: Initial link
+:: https://wall.alphacoders.com/big.php?i=1108174
+:: Download link
+:: https://initiate.alphacoders.com
+::/download/wallpaper/crop-or-stretch
+::/1108174/stretched-1920-1080-1108174.jpg/
+:: 5. Download from link to the path
+:: 6. Set wallpaper from path  
