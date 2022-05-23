f6fa2b35c0c91b5b96565e46b47479a17bcb7717
SDD701 <19keriksov@protonmail.com>
2022-05-23 23:00:11 +0700

    Works, but only for me now

--- SetWallpaper.bat
+++ SetWallpaper.bat
@@ -3,6 +3,7 @@
 Set wallpaperPath = %1
 
 :: 2. Get a link of a random wallpaper
+:loop
 Parser.py > Output
 SET /p downloadLink=<Output
 ::ECHO %downloadLink%
@@ -12,6 +13,20 @@ DEL Output
 :: 3. Download from link to the path
 DEL wallpaper.jpg
 wget --no-check-certificate -O wallpaper.jpg %downloadLink%
+ResolutionTest.py > Output
+SET /p result=<Output
+::ECHO %result%
+::PAUSE
+DEL Output
+if %result%==False GOTO loop
 
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "D:\Projects\Random walpaper fetch win\wallpaper.jpg" /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+::4. Set new wallpaper
+COPY wallpaper.jpg %AppData%\Microsoft\Windows\Themes
+DEL %AppData%\Microsoft\Windows\Themes\TranscodedWallpaper
+REN %AppData%\Microsoft\Windows\Themes\wallpaper.jpg TranscodedWallpaper
+
+COPY wallpaper.jpg %AppData%\Microsoft\Windows\Themes\CachedFiles
+DEL %AppData%\Microsoft\Windows\Themes\CachedFiles\CachedImage_1920_1080_POS2.jpg0
+REN %AppData%\Microsoft\Windows\Themes\CachedFiles\wallpaper.jpg CachedImage_1920_1080_POS2.jpg
+
+POWERSHELL -file RefreshWallpaper.ps1 -path "D:\Projects\Random walpaper fetch win\wallpaper.jpg"
