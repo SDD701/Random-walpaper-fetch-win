b15c80c66aac0ac727ec0b18e5439306f028be88
SDD701 <19keriksov@protonmail.com>
2022-05-23 09:40:56 +0700

    ready

--- Parser.py
+++ Parser.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 URL = "https://wall.alphacoders.com/random.php"
 page = requests.get(URL)
@@ -14,14 +15,19 @@ wallpaperId = line.split("=")
 wallpaperId = wallpaperId[3].split('"')
 wallpaperId = wallpaperId[0]
 
-print(wallpaperId)
+#print(wallpaperId)
 
-URL = "https://wall.alphacoders.com/wallpaper.php?i=" + wallpaperId + "&w=1920&h=1080&type=stretch"
-imageLink2 = "https://wall.alphacoders.com/big.php?i=" + wallpaperId
-print(URL)
+#imageLink = "https://wall.alphacoders.com/wallpaper.php?i=" + wallpaperId + "&w=1920&h=1080&type=stretch"
+URL = "https://wall.alphacoders.com/big.php?i=" + wallpaperId
+#print(URL)
 
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-print(soup)
+soup = str(soup)
+
+downloadLink = str(re.split("picture", soup)[0])
+downloadLink = str(re.split("href", downloadLink)[-1])
+downloadLink = downloadLink.split('"')
+print(downloadLink[1])
