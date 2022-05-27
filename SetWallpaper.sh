3811dd02889c898e907cc7c52a8d954cfb83d7dc
Griffin <griffinhines@comcast.net>
2022-05-27 01:01:55 -0400

    fixed syntax errors

--- SetWallpaper.sh
+++ SetWallpaper.sh
@@ -1,17 +1,12 @@
-@ECHO OFF
+#!/bin/bash
 # 1. Set wallpaper path
 # Set wallpaperPath = %1
 
 # 2. Get a link of a random wallpaper
-result = False
-until [[ $result -eq True ]]; do
-  downloadLink = $(Parser.py)
+downloadLink=$(python Parser.py)
 
-  # 3. Download from link to the path
-  rm wallpaper.jpg
-  wget --no-check-certificate -O wallpaper.jpg %downloadLink%
-  result = $(ResolutionTest.py)
-done
+# 3. Download from link to the path
+wget --no-check-certificate -O wallpaper.jpg $downloadLink
 
 #4. Set new wallpaper
-pywal -i ./wallpaper.jpg
+feh --bg-scale wallpaper.jpg
